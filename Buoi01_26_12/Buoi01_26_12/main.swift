e4f62fb5f9fc58321a2adf51cc265e3006bdf0c3
Ong_Lao_Ngao <onglaongao@OngLaoNgaosMBP.net.fpt>
2019-12-27 01:41:31 +0700

    Bài tập buổi 01

---
+++ Buoi01_26_12/Buoi01_26_12/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Buoi01_26_12
+//
+//  Created by Ong_Lao_Ngao on 12/27/19.
+//  Copyright © 2019 Ong_Lao_Ngao. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
