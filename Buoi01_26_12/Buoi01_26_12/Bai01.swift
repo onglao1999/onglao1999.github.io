f7b6fbafe5fcfd1c7c2da2364a370bc9e1023823
Ong_Lao_Ngao <onglaongao@OngLaoNgaosMBP.net.fpt>
2019-12-27 02:31:49 +0700

    Làm bài 01

---
+++ Buoi01_26_12/Buoi01_26_12/Bai01.swift
@@ -0,0 +1,17 @@
+//
+//  Bai01.swift
+//  Buoi01_26_12
+//
+//  Created by Ong_Lao_Ngao on 12/27/19.
+//  Copyright © 2019 Ong_Lao_Ngao. All rights reserved.
+//
+
+import Foundation
+
+func baiTap01(){
+    print("Nhập bán kính hình cầu: ")
+    let r: Float = Float(readLine() ?? "0") ?? 0
+    print("Diện tích hình cầu có bán kính \(r) là :  \(4 * 3.14 * r)")
+    //print("Thể tích hình cầu có bán kính \(r) là : \( Float(        )")
+}
+
--- Buoi01_26_12/Buoi01_26_12/main.swift
+++ Buoi01_26_12/Buoi01_26_12/main.swift
@@ -7,6 +7,19 @@
 //
 
 import Foundation
+print("                             ----DANH SÁCH MENU----                                ")
+print(" --------------------------------------------------------------------------------")
+print("|    (1) Cho bán kính hình cầu, tính và in ra diện tích, thể tích của hình cầu   |")
+print("|    (2) Tính tổng bình phương 2 số a, b (a, b nhập từ bàn phím)                 |")
+print("|    (3) Viết chương trình nhập vào số nguyên n và xét giá trị của n             |")
+print("|    (4) Cho 2 số a và b, kiểm tra xem a có chia hết cho b không                 |")
+print("|    (6) Giải phương trình bậc nhất                                              |")
+print("|    (7) Chuyển đổi nhiệt độ C sang F và ngược lại                               |")
+print(" --------------------------------------------------------------------------------")
+print("CHỌN BÀI TẬP ( chọn 1 - 7 )")
+let demo: Int = Int( readLine() ?? "0") ?? 0
 
-print("Hello, World!")
 
+if(demo == 1){
+    baiTap01()
+}
