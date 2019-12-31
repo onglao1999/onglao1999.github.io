10010483950a05ea9d5a76dc3f9821f24e3bc145
Ong_Lao_Ngao <onglaongao@OngLaoNgaosMBP.net.fpt>
2019-12-31 22:20:41 +0700

    Bài tự làm trên mạng

---
+++ BT_Tren_Mang/BT_Mang/BT_Mang/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BT_Mang
+//
+//  Created by Ong_Lao_Ngao on 12/31/19.
+//  Copyright © 2019 Ong_Lao_Ngao. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
